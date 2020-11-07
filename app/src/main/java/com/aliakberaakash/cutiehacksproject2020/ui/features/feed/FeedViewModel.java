d97373c0dd42a5c6735cb91fc053d91a7b0f5453
aliakberaakash <ali852609@gmail.com>
2020-11-08 00:12:01 +0600

    converted feedfragment to java

---
+++ app/src/main/java/com/aliakberaakash/cutiehacksproject2020/ui/features/feed/FeedViewModel.java
@@ -0,0 +1,7 @@
+package com.aliakberaakash.cutiehacksproject2020.ui.features.feed;
+
+import androidx.lifecycle.ViewModel;
+
+public class FeedViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/aliakberaakash/cutiehacksproject2020/ui/features/feed/FeedViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.aliakberaakash.cutiehacksproject2020.ui.features.feed
-
-import androidx.lifecycle.ViewModel
-
-class FeedViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
